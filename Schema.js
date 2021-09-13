dd69046b0c127c008c7ea3d01288558ac6a83e39
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-13 17:18:46 +0300

    lab11

---
+++ Schema.js
@@ -0,0 +1,61 @@
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+let BookName
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/book');
+  const BookSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    status: String,
+    email: String,
+  })
+
+  BookName = mongoose.model('bookname', BookSchema);
+  // seedData();
+}
+
+async function seedData() {
+  const almuwatta = new BookName({
+    title: "Al Muwatta",
+    description: " Muwatta Imam Malik of Imam Malik written in the 8th-century, is the earliest collection of hadith texts comprising the subjects of Islamic law, compiled by the Imam, Malik ibn Anas.",
+    status: "avaliable",
+    email: "marwansalem55@yahoo.com",
+  });
+
+  const theRisala = new BookName({
+    title: "The Risāla",
+    description: " book of the communication on the foundations of comprehension  is a seminal text on the principles of Islamic jurisprudence",
+    status: "avaliable",
+    email: "marwansalem55@yahoo.com",
+  });
+  const alMusnad = new BookName({
+    title: "al Musnad",
+    description: " Ahmad ibn Hanbal is a collection of hadith compiled by the Islamic scholar Ahmad ibn Hanbal to whom the Hanbali fiqh is attributed.",
+    status: "avaliable",
+    email: "marwansalem55@yahoo.com",
+  });
+  console.log(BookName)
+  await almuwatta.save();
+  await theRisala.save();
+  await alMusnad.save();
+}
+// main()
+
+
+function getBook(req, res) {
+
+    const frontemail = req.query.frontemail;
+   
+    BookName.find({ email: frontemail }, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.send(result);
+      }
+    })
+  }
+  module.exports=getBook
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 'use strict';
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+let getBook=require('./Schema')
 
-  response.send('test request received')
 
-})
+
+app.get('/books', getBook);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
