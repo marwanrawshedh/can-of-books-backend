4d4d8f456161bdb16c2fe7a45140c55a68d5be39
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-15 16:01:56 +0300

    lab13

--- server.js
+++ server.js
@@ -1,61 +1,74 @@
-'use strict';
-const mongoose = require('mongoose');
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+"use strict";
+const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT;
 app.use(express.json());
-let BookName=require('./Schema')
-let getBook=require('./Getbook')
-mongoose.connect(process.env.URL)
+let BookName = require("./Schema");
+let getBook = require("./Getbook");
+mongoose.connect(process.env.URL);
 
-
-app.get('/books', getBook);
-
-app.get('*', (req, res) => {
-    res.status(404).send('Sorry, page not found');
-  })
+app.get("/books", getBook);
 
 
 
-  app.post("/addbooks", addBooksHandler);
-  async function addBooksHandler(request, response) {
-    const title = request.body.title;
-    const description = request.body.description;
-    const status = request.body.status;
-    const userEmail = request.body.email;
-    await BookName.create({
-      title: title,
-      description: description,
-      status: status,
-      email: userEmail,
-    });
+app.post("/addbooks", addBooksHandler);
+async function addBooksHandler(request, response) {
+  const title = request.body.title;
+  const description = request.body.description;
+  const status = request.body.status;
+  const userEmail = request.body.email;
+  await BookName.create({
+    title: title,
+    description: description,
+    status: status,
+    email: userEmail,
+  });
+  BookName.find({ email: userEmail }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.send(result);
+    }
+  });
+}
+app.delete("/deletebooks/:id", deleteBooksHandler);
+function deleteBooksHandler(req, res) {
+  const bookId = req.params.id;
+  const userEmail = req.query.email;
+  
+  BookName.deleteOne({ _id: bookId }, (err, result) => {
     BookName.find({ email: userEmail }, (err, result) => {
       if (err) {
         console.log(err);
       } else {
-        response.send(result);
+        res.send(result);
       }
     });
-  }
-  app.delete("/deletebooks/:id", deleteBooksHandler);
-  function deleteBooksHandler(req, res) {
-    const bookId = req.params.id;
-    const userEmail = req.query.email;
-    console.log(bookId)
-    console.log(userEmail)
-    BookName.deleteOne({ _id: bookId }, (err, result) => {
-      BookName.find({ email: userEmail }, (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      });
-    });
-  }
+  });
+}
+
+app.put("/updatebooks", update);
+function update(req,res) {
+  const id = req.params.id;
+  const {title,description,status} = req.body;
+  
+  KittenModel.findByIdAndUpdate(id,{title,description,status},(err,result)=>{
+    KittenModel.find({email:email},(err,result)=>{
+      if(err)
+      {
+        console.log(err);
+      }
+      else
+      {
+        res.send(result);
+      }
+    })
+  })
+}
 
 
 
@@ -65,5 +78,7 @@ app.get('*', (req, res) => {
 
 
 
-
+app.get("*", (req, res) => {
+  res.status(404).send("Sorry, page not found");
+});
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
